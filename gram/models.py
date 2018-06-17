8c9d5c9227fa61685c848bc4702d21749f0b76b2
Monroe100 <michelle1hunnid@gmail.com>
2018-06-17 23:59:27 +0300

    create Profile model class in models.py

--- gram/models.py
+++ gram/models.py
@@ -10,16 +10,16 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
-# class Profile(models.Model):
-#     profile_photo = models.ImageField(upload_to='profiles/', null=True)
-#     bio = models.TextField(null=True)
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to='profiles/', null=True)
+    bio = models.TextField(null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-#     def __str__(self):
-#         '''
-#         Display for profiles in profile table
-#         '''
-#         return self.user.username
+    def __str__(self):
+        '''
+        Display for profiles in profile table
+        '''
+        return self.user.username
 
 #     @classmethod
 #     def get_profiles(cls):
