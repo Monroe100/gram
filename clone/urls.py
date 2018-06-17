ccffa53a63a936db6f60ae2d58c5fa5bff0857db
Monroe100 <michelle1hunnid@gmail.com>
2018-06-17 22:12:05 +0300

    create logout url in urls.py

--- clone/urls.py
+++ clone/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('gram.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
-    # url(r'^logout/$', views.logout, {"next_page": '/'}),
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
 
 ]
 if settings.DEBUG:
