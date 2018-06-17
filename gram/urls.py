803736e3c6332642f67a4c7d59d8bc2d0fd7a825
Monroe100 <michelle1hunnid@gmail.com>
2018-06-18 00:08:30 +0300

    create urls.py file in gram

--- gram/urls.py
+++ gram/urls.py
@@ -1,8 +1,8 @@
 
-# from django.conf import settings
-# from django.conf.urls.static import static
-# from django.conf.urls import url
-# from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
+from . import views
 
 # urlpatterns=[
 #     url('^$',views.welcome,name = 'welcome'),
