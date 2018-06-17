ebb0ff4b9916d2b9e6d669616001c83e8a32d65c
Monroe100 <michelle1hunnid@gmail.com>
2018-06-17 23:54:40 +0300

    create form.py file for forms

--- gram/forms.py
+++ gram/forms.py
@@ -1,15 +1,15 @@
-# from django.contrib.auth.forms import AuthenticationForm
-# from .models import Image, Comment, Profile
-# from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from .models import Image, Comment, Profile
+from django import forms
 
 
-# class ProfileForm(forms.ModelForm):
-#     '''
-#     classs that creates a profile update form
-#     '''
-#     class Meta:
-#         model = Profile
-#         fields = ['profile_photo', 'bio', 'user']
+class ProfileForm(forms.ModelForm):
+    '''
+    classs that creates a profile update form
+    '''
+    class Meta:
+        model = Profile
+        fields = ['profile_photo', 'bio', 'user']
 
 
 # class ImagePostForm(forms.ModelForm):
