ea717262652f48b9440710572d383e00a94ddaab
Monroe100 <michelle1hunnid@gmail.com>
2018-06-18 11:15:05 +0300

    made changes in static folder

--- clone/settings.py
+++ clone/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'gram',
-     'bootstrap3',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
