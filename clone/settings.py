efb60558e285a3ce81c908b6ca637886c67acdb9
Monroe100 <michelle1hunnid@gmail.com>
2018-06-18 11:50:46 +0300

    deploying to heroku

--- clone/settings.py
+++ clone/settings.py
@@ -81,12 +81,9 @@ WSGI_APPLICATION = 'clone.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DBNAME'),
-        'USER':config('DBUSER'),
-        'PASSWORD':config('DBPASSWORD')
-   }
+  'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+  )
 }
 
 
