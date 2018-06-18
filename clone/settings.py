0e7eb20f517eac0eb784c79de5a8b571f76e323b
Monroe100 <michelle1hunnid@gmail.com>
2018-06-18 11:37:26 +0300

    deploying to heroku

--- clone/settings.py
+++ clone/settings.py
@@ -68,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -139,4 +140,3 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
